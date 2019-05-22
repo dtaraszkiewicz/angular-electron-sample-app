74509a6720c4adacb01b5b02a9a1ee2a3102d781
Damian Taraszkiewicz <damtar94@gmail.com>
2019-05-22 10:26:57 +0200

    small fix

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -5,7 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit { 
+  title = 'angular-electron-sample-app';
 
   constructor() { }
 
