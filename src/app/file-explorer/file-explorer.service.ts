3c63e1d84e0ae21e02ecde3bf6683b4cd71c0c00
Damian Taraszkiewicz <damtar94@gmail.com>
2019-05-31 14:26:02 +0200

    fileexplorer service refactoring

--- src/app/file-explorer/file-explorer.service.ts
+++ src/app/file-explorer/file-explorer.service.ts
@@ -16,17 +16,14 @@ export class FileExplorerService {
 
   getRoot(): DynamicFlatNode[] {
     return this.fs.readdirSync(`${this.currentPath}`)
-      .map((file: string) =>  this.mapFileToNode(file, file));
+      .map((file: string) => this.mapFileToNode(file, file));
   }
 
   getChildren(node: DynamicFlatNode): DynamicFlatNode[] {
     return this.fs.readdirSync(node.relativePath)
-    .map((file: string) => {
-      if(node.relativePath === node.item) {
-        return this.mapFileToNode(file, `${node.item}/${file}`, node.level+1);
-      }
-      return this.mapFileToNode(file, `${node.relativePath}/${file}`, node.level+1);
-    });
+      .map((file: string) => {
+        return this.mapDependOnLevel(file, node)
+      });
   }
 
   isExpandable(path: string): boolean {
@@ -35,5 +32,12 @@ export class FileExplorerService {
 
   private mapFileToNode(fileName: string, filePath: string, level = 0) {
     return new DynamicFlatNode(fileName, level, this.fs.statSync(filePath).isDirectory(), false, filePath);
+  }
+
+  private mapDependOnLevel(file: string, node: DynamicFlatNode) {
+    if (node.relativePath === node.item) {
+      return this.mapFileToNode(file, `${node.item}/${file}`, node.level + 1);
+    }
+    return this.mapFileToNode(file, `${node.relativePath}/${file}`, node.level + 1);
   }
 }
