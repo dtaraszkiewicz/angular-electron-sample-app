2a6be21b3dc3a6f98fae048543005de8b1ad5b16
dtaraszkiewicz <damtar94@gmail.com>
2019-06-01 19:44:12 +0200

    Feature/3 setup installers (#5)
    * setup windows build and installer
    
    * added builds for all platforms
    
    * user friendly installer
    
    * new line in build.json

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 
+if (require('electron-squirrel-startup')) app.quit();
+// if first time install on windows, do not run application, rather
+// let squirrel installer do its work
+const setupEvents = require('./setup-events');
+if (setupEvents.handleSquirrelEvent()) {
+  process.exit();
+}
+
 let win;
 
 function createWindow () {
@@ -12,14 +20,11 @@ function createWindow () {
       nodeIntegration: true,
 		  backgroundThrottling: false
     }
-  })
-
+  });
 
   win.loadURL(`file://${__dirname}/dist/angular-electron-sample-app/index.html`)
 
   win.maximize();
-
-  win.webContents.openDevTools()
 
   // Event when the window is closed.
   win.on('closed', function () {
@@ -28,20 +33,25 @@ function createWindow () {
 }
 
 // Create window on electron intialization
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow();
+  if (app.isPackaged === false) {
+    win.webContents.openDevTools();
+  }
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
 
   // On macOS specific close process
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
 app.on('activate', function () {
   // macOS specific close process
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 })
